f96bfece215bd4695cef14de79c57ff708892f67
SANTOSH KUMAR SAH <santoshkumar816102@gmail.com>
2023-10-29 15:33:07 +0530

    First Java Project

---
+++ MyFirstJavaCode/src/operators/ArithmeticOperators.java
@@ -0,0 +1,34 @@
+package operators;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+
+		int numOne = 10;
+
+		int numTwo = 5;
+
+		System.out.println(numOne + numTwo); // Output will be 15
+		System.out.println(numOne - numTwo); // Output will be 5
+		System.out.println(numOne * numTwo); // Output will be 50
+		System.out.println(numOne / numTwo); // Output will be 2
+		System.out.println(numOne % numTwo); // Output will be 0
+
+		// in other hand
+
+		int num1 = 10, num2 = 20, sum = 0;
+
+		// Displaying numl and num2
+
+		System.out.println("num1 = " + num1);
+
+		System.out.println("num2 = " + num2);
+
+		// adding num1 and num2
+		sum = num1 + num2;
+
+		System.out.println("The sum = " + sum);
+
+	}
+
+}
